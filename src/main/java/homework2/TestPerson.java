c41083d896eeddbb3290f9fd1473e567f35588ea
dmytrol <dmytrol@wix.com>
2016-08-02 16:06:09 +0300

    Fixed the toString override to fit the requirement

--- src/main/java/homework2/TestPerson.java
+++ src/main/java/homework2/TestPerson.java
@@ -34,7 +34,7 @@ public class TestPerson {
         System.out.println("Is Person3 equal to Person1? \n" + person3.equals(person1));
         System.out.println();
 
-        System.out.println("Is  Address of person3 is equal to address of person2? \n" + person3.getAddress().equals(person2.getAddress()));
+        System.out.println("Is Address of person3 is equal to address of person2? \n" + person3.getAddress().equals(person2.getAddress()));
         System.out.println();
 
         System.out.println("Is  Address of person4 is equal to address of person1? \n" + person4.getAddress().equals(person1.getAddress()));
