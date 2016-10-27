ed77ea0c11d278483d2486b4b52ec4293fae3f9d
dmytrol <dmytrol@wix.com>
2016-10-27 18:55:33 +0300

    added a check for not a number exception

--- src/main/java/homework5/NegativeBalance.java
+++ src/main/java/homework5/NegativeBalance.java
@@ -1,19 +1,20 @@
 package homework5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- NegativeBalanceException
-
- Create BankAccount class which contains:
- an instance variable named userBalance of type double
- constructor that sets the userBalance
- method named withdraw which takes a parameter of type double representing the amount to withdraw.
- and throw a custom NegativeBalanceException if the required amount to withdraw is greater than the userBalance.
-
- Note! the new custom NegativeBalanceException should include a meaningful error message.
-
- Create a program and test BankAccount userBalance & withdraw flows.
+ * NegativeBalanceException
+ * <p>
+ * Create BankAccount class which contains:
+ * an instance variable named userBalance of type double
+ * constructor that sets the userBalance
+ * method named withdraw which takes a parameter of type double representing the amount to withdraw.
+ * and throw a custom NegativeBalanceException if the required amount to withdraw is greater than the userBalance.
+ * <p>
+ * Note! the new custom NegativeBalanceException should include a meaningful error message.
+ * <p>
+ * Create a program and test BankAccount userBalance & withdraw flows.
  */
 public class NegativeBalance {
     private double userBalance;
@@ -44,7 +45,15 @@ public class NegativeBalance {
 
     public static void main(String[] args) {
         System.out.println("Please enter BALANCE of your imaginary bank account: ");
-        NegativeBalance negativeBalance = new NegativeBalance(setInitialUserBalance());
+        double initialBalance = 0;
+
+        try {
+            initialBalance = setInitialUserBalance();
+        } catch (InputMismatchException e) {
+            System.out.println("You entered not a number");
+        }
+
+        NegativeBalance negativeBalance = new NegativeBalance(initialBalance);
         System.out.println();
 
         while (negativeBalance.userBalance > 0) {
@@ -52,7 +61,10 @@ public class NegativeBalance {
                 negativeBalance.withdraw();
             } catch (NegativeBalanceException e) {
                 System.out.println(e.getMessage());
+                System.out.println("Your current balance is " + negativeBalance.userBalance);
                 System.out.println();
+            } catch (InputMismatchException e) {
+                System.out.println("You entered not a number");
             }
         }
     }
